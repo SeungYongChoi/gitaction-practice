6aa14a06dbbfad95f47ce00edc8eff8c2157c5e4
Seungyong Choi <ppyooy336@naver.com>
2022-07-21 13:00:46 +0900

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# 11
+FROM openjdk:11-jre
+
+ENV APP_HOME=/usr/app/
+
+WORKDIR $APP_HOME
+
+COPY build/libs/*.jar application.jar
+
+EXPOSE 8080
+
+CMD ["java", "-jar", "application.jar"]
+
--- src/test/java/gitaction/demo/MemberTest.java
+++ src/test/java/gitaction/demo/MemberTest.java
@@ -12,7 +12,7 @@ class MemberTest {
         Member member = new Member();
         member.setName("yong");
 
-        Assertions.assertThat(member.getName()).isEqualTo("yong2");
+        Assertions.assertThat(member.getName()).isEqualTo("yong");
     }
 
 }
